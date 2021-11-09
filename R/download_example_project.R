2d9aafdc95217d4f0ffb499c565d1f3c47c5a7e1
MatthiasUckert <Matthias.Uckert@outlook.com>
2021-11-09 09:28:35 +0100

    changed example files changed vignette

--- R/download_example_project.R
+++ R/download_example_project.R
@@ -7,7 +7,21 @@
 #' @export
 download_example_project <- function(.dir, .name) {
   dir_ <- system.file("extdata/test_project", package = "RsClassification")
-  fs::dir_copy(dir_, .dir, overwrite = FALSE)
-  file.rename(file.path(.dir, "test_project"), file.path(.dir, .name))
+  check_ <- try(fs::dir_copy(dir_, .dir, overwrite = FALSE), silent = TRUE)
+  
+  if (!inherits(check_, "try-error")) {
+    file.rename(file.path(.dir, "test_project"), file.path(.dir, .name))
+    message("Project successfully created")
+  } else {
+    msg_ <- attr(check_, "condition")$message
+    if (grepl("file already exists", msg_)) {
+      message("Project already exists")
+    } else {
+      message(msg_)
+    }
+    
+  }
+  
+  
   
 }
